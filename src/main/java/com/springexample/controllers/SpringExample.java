384deb9c49684b79bfa9efd8382b0396d32908b0
Derrick <derricksouma@gmail.com>
2015-05-05 11:20:58 +0300

    setup spring mvc

---
+++ src/main/java/com/springexample/controllers/SpringExample.java
@@ -0,0 +1,11 @@
+package com.springexample.controllers;
+
+import org.springframework.stereotype.Controller;
+
+/**
+ * Created by derric on 5/5/15.
+ */
+
+@Controller
+public class SpringExample {
+}
