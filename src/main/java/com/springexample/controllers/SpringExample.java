a5336180bb11b4085a066ec9258acc2bba4dad63
Derrick <derricksouma@gmail.com>
2015-05-05 12:09:48 +0300

    --

--- src/main/java/com/springexample/controllers/SpringExample.java
+++ src/main/java/com/springexample/controllers/SpringExample.java
@@ -1,6 +1,9 @@
 package com.springexample.controllers;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
 
 /**
  * Created by derric on 5/5/15.
@@ -8,4 +11,22 @@ import org.springframework.stereotype.Controller;
 
 @Controller
 public class SpringExample {
+    
+    @RequestMapping("/welcome")
+    public String hello(Model model){
+
+        model.addAttribute("message","HELLO WORLD");
+        return "hello";
+    }
+
+    @RequestMapping("/index")
+    public ModelAndView hello1(){
+
+        ModelAndView model = new ModelAndView();
+        model.addObject("msg","WELCOME TO SPRING MVC");
+        model.setViewName("hello");
+
+        return model;
+    }
+    
 }
